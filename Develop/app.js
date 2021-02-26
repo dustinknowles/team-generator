34d7c91e286454bf71622685ea1267ab51af873f
dustin.knowles <dustin.knowles22@gmail.com>
2021-02-25 16:41:50 -0800

    adding populated employee file

--- Develop/app.js
+++ Develop/app.js
@@ -1,37 +1,184 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
+const jest = require("jest");
 const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
+//Module Imports
+const Employee = require("./lib/Employee.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/Manager.js");
+const generateHTML = require("./output/generateHTML.js");
+
+//Express app setup
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Adds urlencoding and parses json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Employees DATA
+const employees = [];
+const engineers = [];
+const interns = [];
+const managers = [];
+let id = 0;
+var response;
+
+//Prompts user, uses switch-case to allow for each job property
+const promptUser = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Name:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Email:",
+        name: "email",
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "What's Your/Their Position At The Company?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+    ])
+    .then(function (data) {
+      switch (data.role) {
+        case "Manager":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "Enter employee ID: ",
+                name: "id",
+              },
+              {
+                type: "input",
+                message: "Enter office number:",
+                name: "office",
+              },
+            ])
+            .then(function (res) {
+              const officeNum = res.office;
+              console.log(officeNum);
+              const manager = new Manager(
+                data.name,
+                res.id,
+                data.email,
+                officeNum,
+                "Manager"
+              );
+              console.log(manager);
+              employees.push(manager);
+            })
+            .then(function () {
+              addNext();
+            });
+          break;
+        case "Engineer":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "Enter employee ID: ",
+                name: "id",
+              },
+              {
+                type: "input",
+                message: "Enter github username: ",
+                name: "github",
+              },
+            ])
+            .then(function (res) {
+              const githubName = res.github;
+              const engineer = new Engineer(
+                data.name,
+                res.id,
+                data.email,
+                githubName,
+                "Engineer"
+              );
+              employees.push(engineer);
+            })
+            .then(function () {
+              addNext();
+            });
+          break;
+        case "Intern":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "Enter employee ID: ",
+                name: "id",
+              },
+              {
+                type: "input",
+                message: "Enter school: ",
+                name: "school",
+              },
+            ])
+            .then(function (res) {
+              const internSchool = res.school;
+              const intern = new Intern(
+                data.name,
+                res.id,
+                data.email,
+                internSchool,
+                "Intern"
+              );
+              employees.push(intern);
+            })
+            .then(function () {
+              addNext();
+            });
+          break;
+      }
+    })
+    .then(function () {});
+};
+
+const addNext = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "add",
+        message: "Would you like to add another employee?",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then(function (res) {
+      if (res.add === "Yes") {
+        promptUser();
+      } else {
+        console.log("Done");
+        completedRoster(employees);
+      }
+    });
+};
+
+function completedRoster(employees) {
+  console.log("Success!");
+  console.log(employees);
+  const html = generateHTML(employees);
+  console.log(html);
+  writeFileAsync("./output/employees.html", html, "utf-8");
+}
+
+function init() {
+  console.log("Please enter employee info");
+  promptUser();
+}
+
+init();
